489e23fb66f15c31ca2184ebf19b999bff441f39
StefanTheCode <stefan.djokic1995@gmail.com>
2023-09-07 10:54:47 +0200

    precedence

--- app/head.tsx
+++ app/head.tsx
@@ -4,7 +4,7 @@ export default function Head() {
             <title>Stefan Djokic</title>
             <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
             <link rel="icon" href="/favicon.ico" />
-            <link rel="stylesheet" precedence="default" href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900" />
+            <link rel="stylesheet"  href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900" />
         </>
     )
 }
