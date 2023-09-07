079ef2000f3c14c7de07aa0c1cbe5c1153eb290c
StefanTheCode <stefan.djokic1995@gmail.com>
2023-09-07 11:10:06 +0200

    subscribeNewsletter

---
+++ components/subscribeNewsletter.tsx
@@ -0,0 +1,13 @@
+import Script from 'next/script';
+
+const SubscribeNewsletter = () => (
+  <div className="col-xs-4 col-sm-12 col-md-8 col-lg-8 col-xl-12 text-center octopus-input-margin-left" >
+    <Script 
+      async 
+      src="https://eocampaign1.com/form/33e483be-a0b1-11ed-b1bd-9b9d338510f2.js"
+      data-form="33e483be-a0b1-11ed-b1bd-9b9d338510f2">
+    </Script>
+    </div>
+);
+
+export default SubscribeNewsletter;
