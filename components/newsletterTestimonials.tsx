6040d2d48134aed786a570145bb9b18301fbe3b3
StefanTheCode <stefan.djokic1995@gmail.com>
2023-09-07 11:34:10 +0200

    Widget fix

--- components/newsletterTestimonials.tsx
+++ components/newsletterTestimonials.tsx
@@ -2,17 +2,13 @@ import Script from 'next/script';
 
 const NewsletterTestimonials = () => (
   <div className="col-md-12">
-    <div 
-      className="senja-embed"
-      data-id="3aab87ef-ff9d-4582-a844-a6fc1b3b35e4"
-      data-lazyload="false">
-    </div>
-    <Script 
-      src="https://static.senja.io/dist/platform.js" 
-      async 
-      type="text/javascript">
-    </Script>
-  </div>
+  <iframe 
+    src="https://widget.senja.io/widget/3aab87ef-ff9d-4582-a844-a6fc1b3b35e4"
+    width="100%"
+    height="250vw"
+    frameBorder="0">
+  </iframe>
+</div>
 );
 
 export default NewsletterTestimonials;
