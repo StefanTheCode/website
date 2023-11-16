cd41a1632662cc283bc20bb0e24d886925c08e55
StefanTheCode <stefan.djokic1995@gmail.com>
2023-11-16 23:08:19 +0100

    ebook

--- app/ebook/page.tsx
+++ app/ebook/page.tsx
@@ -17,7 +17,7 @@ const Ebook = () => {
               <p className="header-text mtopminus10"><span className='text-yellow'>SIMPLIFIED</span> </p>
               <p className="mb-4 text-white"><b>Go-to resource for understanding the <span className='text-yellow'>core concepts of design patterns</span> without the overwhelming complexity. In this concise and affordable ebook, I've distilled the essence of design patterns into an easy-to-digest format.</b></p>
               <p className="mb-4 text-white"><b>Join <span className='text-yellow'>{config.EbookCopiesNumber} engineers</span>  to master design patterns the simplified way!</b></p>
-              <a href='/'><button className='btn btn-lg btn-primary border-radius-10px button-padding'>Download for <span className='text-green'><b> $10</b></span></button></a>
+              <a href='https://stefandjokic.lemonsqueezy.com/checkout'><button className='btn btn-lg btn-primary border-radius-10px button-padding'>Download for <span className='text-green'><b> $10</b></span></button></a>
             </div>
           </div>
           <div className="col-xl-6 col-lg-12 col-md-12 col-sm-12 col-xs-12 float-right">
@@ -38,7 +38,7 @@ const Ebook = () => {
           <div className="row text-center">
             <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12 mb-5 text-center">
               <SponsorsNewsletter />
-              <a href='/'><button className='btn btn-lg btn-primary border-radius-10px mt-5 button-padding'>Download for <span className='text-green'><b> $10</b></span></button></a>
+              <a href='https://stefandjokic.lemonsqueezy.com/checkout'><button className='btn btn-lg btn-primary border-radius-10px mt-5 button-padding'>Download for <span className='text-green'><b> $10</b></span></button></a>
             </div>
           </div>
         </div>
@@ -87,7 +87,7 @@ const Ebook = () => {
               </div>
             </div>
             <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12 mb-5 text-center">
-              <a href='/'><button className='btn btn-lg btn-primary border-radius-10px mt-5 button-padding'>Download for <span className='text-green'><b> $10</b></span></button></a>
+              <a href='https://stefandjokic.lemonsqueezy.com/checkout'><button className='btn btn-lg btn-primary border-radius-10px mt-5 button-padding'>Download for <span className='text-green'><b> $10</b></span></button></a>
             </div>
           </div>
         </div>
@@ -123,7 +123,7 @@ const Ebook = () => {
                   <h5>10. Strategy</h5>
                 </div>
                 <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12 mb-5 text-center">
-                  <a href='/'><button className='btn btn-lg btn-primary border-radius-10px mt-5 button-padding'>Download for <span className='text-green'><b> $10</b></span></button></a>
+                  <a href='https://stefandjokic.lemonsqueezy.com/checkout'><button className='btn btn-lg btn-primary border-radius-10px mt-5 button-padding'>Download for <span className='text-green'><b> $10</b></span></button></a>
                 </div>
               </div>
             </div>
@@ -145,7 +145,7 @@ const Ebook = () => {
               <h5 className='text-left'>• Solve real-world challenges.</h5>
               <h5 className='text-left'>• Enhance coding skills for robust applications.</h5>
               <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12 mb-5 text-center">
-                <a href='/'><button className='btn btn-lg btn-primary border-radius-10px mt-5 button-padding'>Download for <span className='text-green'><b> $10</b></span></button></a>
+                <a href='https://stefandjokic.lemonsqueezy.com/checkout'><button className='btn btn-lg btn-primary border-radius-10px mt-5 button-padding'>Download for <span className='text-green'><b> $10</b></span></button></a>
               </div>
             </div>
           </div>
@@ -155,12 +155,86 @@ const Ebook = () => {
               <h2 className='text-yellow text-center'><b>Free GitHub Repository</b></h2>
               <h5 className='pt-5'>In addition to the code found in the book itself, as accompanying material comes a free GitHub repository with implementations of all 10 patterns in the C# programming language.</h5>
               <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12 mb-5 text-center">
-                <a href='/'><button className='btn btn-lg btn-primary border-radius-10px mt-5 button-padding'>Download for <span className='text-green'><b> $10</b></span></button></a>
+                <a href='https://stefandjokic.lemonsqueezy.com/checkout'><button className='btn btn-lg btn-primary border-radius-10px mt-5 button-padding'>Download for <span className='text-green'><b> $10</b></span></button></a>
               </div>
             </div>
             <div className="col-xl-6 col-lg-6 col-md-6 col-sm-6 mb-5 text-center vertical-center">
               <img src="/images/github-repo.png" className='border-radius-20px' alt="Profile image of Stefan Djokic" width="100%"/>
             </div>
+          </div>
+        </div>
+      </section>
+
+     
+      <section className="ftco-section background-yellow " id="newsletter-section">
+        <div className="container">
+        <div className="row justify-content-center mb-5">
+            <div className="col-md-12 heading-section text-center">
+              <h2 className="text-black"><b>Who am I?</b></h2>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-md-3">
+            </div>
+            <div className="col-md-6">
+              <div className="row">
+                <div className="col-md-6 ebook-profile-div">
+                <img src="/images/ebook-stefan.png" className='mb-5 border-radius-20px ebook-profile-img' alt="Profile image of Stefan Djokic" />
+                </div>
+                <div className="col-md-6 ebook-about-div">
+                  <p className='text-black text-font-2rem'><b>Hi friend, I'm Stefan</b></p>
+                  <p className='text-black'>I am a senior software engineer with years of industry experience. I help a large number of developers to become better in their daily work through the content I share on social networks, blog and newsletter.</p>
+                </div>
+                <div className="col-md-12">
+                  <p className='text-black'><i><b>"Keep it simple and focus on what matters. Don't let yourself be overwhelmed."</b></i> - Confucius</p>
+                  <p className='text-black text-center'>My goal is to convey knowledge to people in such a way - <b>simple.</b></p>
+                  </div>
+              </div>
+            </div>
+            <div className="col-md-3">
+            </div>
+          </div>
+          <div className="row mt-5">
+            <div className="col-md-4">
+              <div className="row">
+                <div className="col-md-12 text-center">
+                  <p className='text-black text-font-2rem'>Newsletter</p>
+                  <p className='text-black text-font-2rem'><b>{config.NewsletterSubCount}</b></p>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-4">
+              <div className="row">
+                <div className="col-md-12 text-center">
+                  <p className='text-black text-font-2rem'>Linkedin</p>
+                  <p className='text-black text-font-2rem'><b>{config.LinkedinFollowers}</b></p>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-4">
+              <div className="row">
+                <div className="col-md-12 text-center">
+                  <p className='text-black text-font-2rem'>Twitter</p>
+                  <p className='text-black text-font-2rem'><b>{config.TwitterFollowers}</b></p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      
+      <section className="ftco-section" id="blog-section">
+        <div className="container">
+          <div className="row justify-content-center mb-5">
+            <div className="col-md-12 heading-section text-center">
+              <p className="header-text"><span className='text-yellow'>Keep It Simple </span> with Design Patterns</p>
+            </div>
+          </div>
+          <div className="row text-center">
+            <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12 mb-5 text-center">
+            <a href='https://stefandjokic.lemonsqueezy.com/checkout'><button className='btn btn-lg btn-primary border-radius-10px mt-5 button-padding'>Download for <span className='text-green'><b> $10</b></span></button></a>
+        </div>
           </div>
         </div>
       </section>
