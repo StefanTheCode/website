69630cd0823e08a6c61aa5ee51b3acd56679664b
StefanTheCode <stefan.djokic1995@gmail.com>
2023-12-09 17:30:57 +0100

    FIx

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -2,6 +2,7 @@ import getPostMetadata from "@/components/getPostMetadata";
 import Subscribe from "../subscribe";
 import PostPreview from "@/components/PostPreview";
 import { Metadata } from "next";
+import config from '@/config.json'
 
 export const metadata:Metadata = {
   title: "Stefan Djokic | Blog",
@@ -24,7 +25,7 @@ const Blog = () => {
 
   return (
     <>
-      <section className="img ftco-section ">
+      <section className="img ftco-section">
         <div className="container">
           <div className="row justify-content-center pb-5">
             <div className="col-xs-7 col-sm-7 col-md-7 col-lg-7 heading-section text-center ">
@@ -32,7 +33,30 @@ const Blog = () => {
               <h2 className="mb-4 mt-10">Blog posts</h2>
             </div>
           </div>
-          {postPreviews}
+          <div className="row">
+            <div className="col-md-12">
+            {postPreviews}
+            </div>
+            {/* <div className="col-md-3">
+            <div className="row justify-content-center pb-5">
+            <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12 sticky-top">
+              <h3 className="newsletter-post-title">Subscribe to <br/>.NET Pro Weekly</h3>
+              <p className="text-slate-400 mt-2">Subscribe to the .NET Pro Weekly and be among the <span className="text-yellow"> {config.NewsletterSubCount}</span> gaining practical tips and resources to enhance your .NET expertise.</p>
+              <div className="row">
+              <div className="col-md-12"
+              dangerouslySetInnerHTML={{
+                __html: `
+              <script async src="https://eocampaign1.com/form/e85a08a0-d239-11ed-bf00-69996e57973d.js"
+                data-form="e85a08a0-d239-11ed-bf00-69996e57973d">
+              </script>
+            `
+              }}
+            />
+              </div>
+            </div>
+          </div>
+            </div> */}
+          </div>
         </div>
       </section>
       <hr />
--- app/design-patterns-simplified/page.tsx
+++ app/design-patterns-simplified/page.tsx
@@ -16,7 +16,7 @@ const Ebook = () => {
     <>
       <section id="home-section" className="hero container">
         <div className="row d-md-flex no-gutters">
-          <div className='col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-12 slider-text text-center'>
+          <div className='col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-12 slider-text text-center mt-10'>
             <p className="mb-4 header-sub-text">Who still wants to read a 500+ page book that costs over $100?<br />Instead, dive into...</p>
           </div>
           <div className="col-xl-6 col-lg-12 col-md-12 col-sm-12 col-xs-12 slider-text pt-5 float-left">
