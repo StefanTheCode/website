a325f8d05340d0dcc6863ad3e8ca544728526ab6
StefanTheCode <stefan.djokic1995@gmail.com>
2024-02-06 14:56:04 +0100

    Blog fix

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -91,7 +91,7 @@ const PostPage = (props: any) => {
   return (
     <>
       <section className="img ftco-section">
-        {/* <div className="container"> */}
+        <div className="container">
           <div className="row justify-content-center pb-5 pt-10">
             <div className="col-xs-12 col-sm-12 col-md-12 col-lg-9 col-xl-9 heading-section text-justify border-right">
               <div className="row justify-content-center pb-5">
@@ -125,7 +125,7 @@ const PostPage = (props: any) => {
               </div>
             </div>
           </div>
-        {/* </div>   */}
+        </div>  
       </section >
     
     </>
