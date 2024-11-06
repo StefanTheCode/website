dba9bd5eb2df85a6b9595581631608dbc07294c6
stefan.djokic@exlrt.com <stefan.djokic@exlrt.com>
2024-11-06 22:57:51 +0100

    black-friday

--- app/black-friday/page.tsx
+++ app/black-friday/page.tsx
@@ -8,79 +8,118 @@ const BlackFriday = () => {
         <div className="container">
           <div className='row pb-5 pt-5'>
             <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-              <h1 className='sponsorship-name text-center'>Sponsorship opportunities</h1>
+              <h1 className='sponsorship-name text-center'><b className='text-yellow'>Black Friday</b><br/> Sponsorship opportunities</h1>
               <div className="row">
                 <div className="col-md-4 mt-5 mb-5 padding-right-45px position-relative">
                 <img src="/images/discount_40.png" alt="Discount Tag" className="discount-image"/>
                   <ul className="price">
                     <li className="header"><b>LinkedIn Post</b></li>
-                    <li className="grey padding-5 text-success"><span className='old-price'>$500.00</span>$300.00</li>
+                    <li className="grey padding-5 text-success"><span className='old-price'>$750.00</span>$450.00</li>
                     <li className='padding-5'><h5><b className='text-yellow'>What you'll get?</b></h5></li>
                     <li className='padding-5'>Complete post about your company, product or a service</li>
                     <li className='padding-5'>1+ URLs to your website</li>
                     <li className='padding-5'>Your image, carousel or video</li>
+                    <li className="grey padding-5">
+                      <button className="btn btn-primary border-radius-5px coming-soon">Reserve here</button>
+                    </li>
                     <li className='padding-5'><h5><b className='text-yellow'>Stats</b></h5></li>
 
                     <li className='padding-5'>Average post reach: ~50k people</li>
-                    <li className='padding-5'>Number of followers: ~60k</li>
-                    <li className='padding-5'>Growth: ~1500 - 3000 followers weekly</li>
+                    <li className='padding-5'>Number of followers: ~88k</li>
+                    <li className='padding-5'>Growth: ~1000 - 1500 followers weekly</li>
                     <li className='padding-5'>Profile reach last month: ~ 2.5m views</li>
                     <li className='padding-5'>Average weekly profile reach: ~ 450k - 650k</li>
-                    <li className='padding-5'><h5><b className='text-yellow'>Examples</b></h5></li>
-                    <li className='padding-5'><a href='https://www.linkedin.com/posts/djokic-stefan_i-was-a-full-stack-developer-what-did-activity-7089598495766044673-xg8M/?utm_source=share&utm_medium=member_desktop' target='_blank'>Sponsored post - 41k reach</a></li>
-                    <li className='padding-5'><a href='https://www.linkedin.com/posts/djokic-stefan_dotnet-softwareengineering-workflowautomation-activity-7127638947991552000-NI9K?utm_source=share&utm_medium=member_desktop' target='_blank'>Sponsored post - 70k reach</a></li>
-                    <li className="grey">
-                      <h5>Reserve here</h5>
-                      <h5 className='mail-address'><a href="mailto:stefan@stefandjokic.tech">{config.Email}</a></h5>
+                    <li className="grey padding-5">
+                      <button className="btn btn-primary border-radius-5px coming-soon">Reserve here</button>
                     </li>
-                  </ul>
+                   </ul>
                 </div>
                 <div className="col-md-4 mt-5 mb-5 padding-right-45px position-relative">
-                <img src="/images/discount_33.png" alt="Discount Tag" className="discount-image"/>
+                <img src="/images/discount_25.png" alt="Discount Tag" className="discount-image"/>
                   <ul className="price">
                     <li className="header"><b>Newsletter Ad</b></li>
-                    <li className="grey padding-5 text-success"><span className='old-price'>$300.00</span>$200.00</li>
+                    <li className="grey padding-5 text-success"><span className='old-price'>$400.00</span>$300.00</li>
                     <li className='padding-5'><h5><b className='text-yellow'>What you'll get?</b></h5></li>
                     <li className='padding-5'>1 of maximum 2 sponsors per issue</li>
-                    <li className='padding-5'>Sponsorship will remain live forever on website</li>
+                    <li className='padding-5'>Sponsorship remains live forever on website</li>
                     <li className='padding-5'>2 links per sponsorship</li>
-                    <li className='padding-5'>2 sentences maximum</li>
-                    <li className='padding-5'><h5><b className='text-yellow'>Stats</b></h5></li>
-                    <li className='padding-5'>Reach: 9400+ subscribers</li>
-                    <li className='padding-5'>Growth: ~1200+ subscribers monthly</li>
-                    <li className='padding-5'>Average Open Rate: 46%</li>
-                    <li className='padding-5'>Ad Clicks Average: 100 - 300</li>
-                    <li className='padding-5'><h5><b className='text-yellow'>Examples</b></h5></li>
-                    <li className='padding-5'><a href='https://thecodeman.net/posts/improve-ef-core-performance-with-compiled-queries' target='_blank'>#42 Stefan's Newsletter</a></li>
-                    <li className='padding-5'><a href='https://thecodeman.net/posts/background-tasks-how-to-use-them' target='_blank'>#38 Stefan's Newsletter</a></li>
-                    <li className="grey">
-                      <h5>Reserve here</h5>
-                      <h5 className='mail-address'><a href="mailto:stefan@stefandjokic.tech">{config.Email}</a></h5>
+                    <li className="grey padding-5">
+                      <button className="btn btn-primary border-radius-5px coming-soon">Reserve here</button>
                     </li>
-                  </ul>
+                    <li className='padding-5'><h5><b className='text-yellow'>Stats</b></h5></li>
+                    <li className='padding-5'>Reach: 14,000+ subscribers</li>
+                    <li className='padding-5'>Growth: ~800+ subscribers monthly</li>
+                    <li className='padding-5'>Average Open Rate: 53%</li>
+                    <li className='padding-5'>Ad Clicks Average: 100 - 300</li>
+                    <li className="grey padding-5">
+                      <button className="btn btn-primary border-radius-5px coming-soon">Reserve here</button>
+                    </li>
+                    </ul>
                 </div>
                 <div className="col-md-4 mt-5 mb-5 padding-right-45px position-relative">
                 <img src="/images/discount_60.png" alt="Discount Tag" className="discount-image"/>
                   <ul className="price">
-                    <li className="header"><b>Twitter Post</b></li>
+                    <li className="header"><b>X Post</b></li>
                     <li className="grey padding-5 text-success"><span className='old-price'>$250.00</span>$100.00</li>
                     <li className='padding-5'><h5><b className='text-yellow'>What you'll get?</b></h5></li>
                     <li className='padding-5'>Complete post about your company, product or a service</li>
                     <li className='padding-5'>1+ URLs to your website</li>
                     <li className='padding-5'>Your image, document or video</li>
+                    <li className="grey padding-5">
+                      <button className="btn btn-primary border-radius-5px coming-soon">Reserve here</button>
+                    </li>
                     <li className='padding-5'><h5><b className='text-yellow'>Stats</b></h5></li>
-                    <li className='padding-5'>Average post reach: ~10k people</li>
-                    <li className='padding-5'>Number of followers: ~3k</li>
+                    <li className='padding-5'>Average post reach: ~5k people</li>
+                    <li className='padding-5'>Number of followers: ~5.2k</li>
                     <li className='padding-5'>Growth: 200 - 400 followers weekly</li>
                     <li className='padding-5'>Profile reach last month: ~ 320k views</li>
-                    <li className='padding-5'><h5><b className='text-yellow'>Examples</b></h5></li>
-                    <li className='padding-5'><a href='https://twitter.com/TheCodeMan__/status/1727082995075391525' target='_blank'>Sponsored post - 12k reach</a></li>
-                    <li className="grey">
-                      <h5>Reserve here</h5>
-                      <h5 className='mail-address'><a href="mailto:stefan@stefandjokic.tech">{config.Email}</a></h5>
+                    <li className="grey padding-5">
+                      <button className="btn btn-primary border-radius-5px coming-soon">Reserve here</button>
                     </li>
-                  </ul>
+                    </ul>
                 </div>
+              </div>
+            </div>
+          </div>
+          <div className="d-flex">
+            <div className="col-sm-12 col-md-12 col-lg-12 pb-5">
+            <h2 className='text-center'>Examples</h2>
+             <div className='row'>
+              <div className='col-sm-4 col-md-4 col-lg-4'>
+              <h3 className='pt-5'><b className='text-yellow'>LinkedIn</b></h3>
+              <div className='row'>
+                <div className='col-sm-12 col-md-12 col-lg-12 text-center'>
+                  <a target='_blank' href='https://www.linkedin.com/posts/djokic-stefan_postman-visualstudiocode-activity-7224410047810854913-hNJ1/?utm_source=share&utm_medium=member_desktop'>Postman - 100k reach</a> <br/>
+                  <a target='_blank' href='https://www.linkedin.com/posts/djokic-stefan_how-to-build-api-without-coding-visually-activity-7226532036063240192-vu7i/?utm_source=share&utm_medium=member_desktop'>Postman - 75k reach</a><br/>
+                  <a target='_blank' href='https://www.linkedin.com/posts/djokic-stefan_you-need-skills-and-tools-to-build-a-house-activity-7232738958940528640-OrGZ/?utm_source=share&utm_medium=member_desktop'>Packt - 42k reach</a>
+                </div>
+              </div>
+              </div>
+              <div className='col-sm-4 col-md-4 col-lg-4'>
+              <h3 className='pt-5'><b className='text-yellow'>Newsletter</b></h3>
+              <div className='row'>
+                <div className='col-sm-12 col-md-12 col-lg-12 text-center'>
+                  <a target='_blank' href='https://eomail4.com/web-version?p=a57fbf8c-8545-11ef-ae7d-85df232e2a82&pt=campaign&t=1728379993&s=784b945bc08f7b01ba1243c91db3cc19b19e59426863f51d69a39e55bbd61185'>In newsletter</a> <br/>
+                  <a target='_blank' href='https://thecodeman.net/posts/how-to-use-singleton-in-multithreading'>On website</a><br/>
+                </div>
+              </div>
+              </div>
+               <div className='col-sm-4 col-md-4 col-lg-4'>
+              <h3 className='pt-5'><b className='text-yellow'>X (Twitter)</b></h3>
+              <div className='row'>
+                <div className='col-sm-12 col-md-12 col-lg-12 text-center'>
+                  <a target='_blank' href='https://x.com/TheCodeMan__/status/1841461611803901957'>Packt - 4.8k</a> <br/>
+                  <a target='_blank' href='https://x.com/TheCodeMan__/status/1839580695850611135'>Postman - 4.5k</a><br/>
+                </div>
+              </div>
+              </div>
+             </div>
+              <br />
+              <div className='row'>
+              <div className='col-sm-12 col-md-12 col-lg-12 text-center pb-5 mt-5'>
+               <button className="btn btn-lg btn-primary border-radius-5px" data-eo-form-toggle-id="9ade17e6-9c87-11ef-86b3-890d9e639bbe"></button>
+               <a data-eo-form-toggle-id="9ade17e6-9c87-11ef-86b3-890d9e639bbe" href="#">Reserve here</a>
+            </div>
               </div>
             </div>
           </div>
@@ -91,7 +130,11 @@ const BlackFriday = () => {
                   <h2 className='text-center'>Previous Sponsors</h2>
                   <h2 className='pt-5'><b className='text-yellow'>{config.PreviousSponsors}</b></h2>
                   <br />
-                  <h5>Reserve your sponsorship by sending me an email: <a href="mailto: stefan@stefandjokic.tech">{config.Email}</a></h5>
+                  <div className='row'>
+              <div className='col-sm-12 col-md-12 col-lg-12 text-center pb-5 mt-5'>
+               <button className="btn btn-lg btn-primary border-radius-5px">Reserve here</button>
+            </div>
+              </div>
                 </div>
                 <SponsorsNewsletter />
               </div>
@@ -99,7 +142,13 @@ const BlackFriday = () => {
           </div>
         </div>
       </section>
-
+      <div dangerouslySetInnerHTML={{
+                    __html: `
+                <script async src="https://eocampaign1.com/form/33e483be-a0b1-11ed-b1bd-9b9d338510f2.js"
+								data-form="33e483be-a0b1-11ed-b1bd-9b9d338510f2"></script>
+            `
+                  }}>
+     </div>
     </>
   )
 }
