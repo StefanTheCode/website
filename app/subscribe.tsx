016357e37517db527d6a67ed45d7c05da0b0d437
stefan.djokic@exlrt.com <stefan.djokic@exlrt.com>
2024-12-06 18:46:44 +0100

    formb

--- app/subscribe.tsx
+++ app/subscribe.tsx
@@ -20,9 +20,7 @@ export default function Subscribe() {
             <div className="col-md-8"
               dangerouslySetInnerHTML={{
                 __html: `
-              <script async src="https://eocampaign1.com/form/e85a08a0-d239-11ed-bf00-69996e57973d.js"
-                data-form="e85a08a0-d239-11ed-bf00-69996e57973d">
-              </script>
+             <script async src="https://eomail4.com/form/861505f8-b3f8-11ef-896f-474a313dbc14.js" data-form="861505f8-b3f8-11ef-896f-474a313dbc14"></script>
             `
               }}
             />
