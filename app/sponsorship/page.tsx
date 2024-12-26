8758c0345ea5f6e35499de7cca291f46ab05c4a7
stefan.djokic@exlrt.com <stefan.djokic@exlrt.com>
2024-12-27 00:32:32 +0100

    sponsorship

--- app/sponsorship/page.tsx
+++ app/sponsorship/page.tsx
@@ -2,7 +2,7 @@ import SponsorsNewsletter from '@/components/sponsorsTestimonials';
 import config from '@/config.json'
 import { Metadata } from 'next';
 
-export const metadata:Metadata = {
+export const metadata: Metadata = {
   metadataBase: new URL('https://thecodeman.net/sponsorship'),
   title: "Sponsorship",
   description: "Partner with Stefan Djokic for impactful sponsorship opportunities in our widely-read newsletter and popular blog. A unique platform for companies to showcase their products to a dedicated audience of tech enthusiasts and professionals in the software engineering and .NET community.",
@@ -11,8 +11,8 @@ export const metadata:Metadata = {
     type: "website",
     url: "https://thecodeman.net/sponsorship",
     description: "Partner with Stefan Djokic for impactful sponsorship opportunities in our widely-read newsletter and popular blog. A unique platform for companies to showcase their products to a dedicated audience of tech enthusiasts and professionals in the software engineering and .NET community."
-   },
-   twitter: {
+  },
+  twitter: {
     title: "Sponsorship",
     card: "summary_large_image",
     site: "@TheCodeMan__",
@@ -22,106 +22,93 @@ export const metadata:Metadata = {
 }
 
 const Sponsorship = () => {
-    return (
-      <>
-       <section className="ftco-about img ftco-section ftco-no-pb sponsorship-header">
-    <div className="container">
-      <div className="pb-5">
-        <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12 heading-section mt-5">
-          <h2 className="mb-4 text-align-center">TheCodeMan .NET Newsletter <br/>
-          <span className="text-yellow"><b>Sponsorship</b></span>
-          </h2>
-        </div>
-      </div>
-      <div className="d-flex">
-        <div className="col-sm-12 col-md-12 col-lg-12 pb-5">
-          <div className="row pb-3">
-            <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-              <h1>Audience</h1>
-              <br/>
-              <h5>• Reach: <span  className="text-yellow">{config.NewsletterSubCount}</span></h5>
-              {/* <h5>• Growth: <span  className="text-yellow">{config.Growth}</span></h5> */}
-              {/* <h5>• Number of issues so far: <span  className="text-yellow">{config.IssuesCount}</span></h5> */}
-              <h5>• Average Open Rate: <span  className="text-yellow">{config.OpenRate}</span></h5>
-              <h5>• Audience: <span  className="text-yellow">{config.Audience} </span></h5>
-              <br/>
-              <h5>Reserve your sponsorship by sending me an email: <a href="mailto: stefan@stefandjokic.tech">{config.Email}</a></h5>
-             {/* <h5><b>Currently booked out 2 weeks</b></h5> */}
-              <h5>Previous Sponsors: <b>{config.PreviousSponsors}</b></h5>
-            </div>
-            <SponsorsNewsletter />
-          </div>
-          <div className="row pb-3 mt-5">
-            <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-              <h1 className='sponsorship-name'>Sponsorship opportunities</h1>
-              <div className="row">
-              <div className="col-md-4 mt-5 mb-5">
-                <ul className="price">
-                  <li className="header"><b>Shared Sponsorship</b></li>
-                  <li className="grey" >{config.SharedPrice}</li>
-                  <li>1 of maximum 2 sponsors per issue</li>
-                  <li>Sponsorship will remain live forever on website</li>
-                  <li>2 links per sponsorship</li>
-                  <li>2 sentences maximum</li>
-                  <li className="grey" >
-                  <a className="btn btn-primary border-radius-5px coming-soon" href='#reserveSpotForm'> Reserve here</a>
-                  </li>
-                </ul>
-              </div>
-              <div className="col-md-4  mb-5">
-                <ul className="price">
-                  <li className="header"><b>Exclusive Sponsorship</b></li>
-                  <li className="grey">{config.ExclusivePrice}</li>
-                  <li>Only your sponsorship per issue</li>
-                  <li>Sponsorship will remain live forever on website</li>
-                  <li>Logo included</li>
-                  <li>2 links per sponsorship</li>
-                  <li>2 sentences maximum</li>
-                  <li className="grey" >
-                  <a className="btn btn-primary border-radius-5px coming-soon" href='#reserveSpotForm'> Reserve here</a>
-                  </li>
-                </ul>
-              </div>
-              <div className="col-md-4  mb-5">
-               <p> Available once a month only</p>
-                <ul className="price">
-                  <li className="header"><b>Sponsored Content</b></li>
-                  <li className="grey">{config.SponsoredPrice}</li>
-                  <li>**Article or tutorial related to .NET or C#</li>
-                  <li>Sponsorship will remain live forever on website</li>
-                  <li>Logo included</li>
-                  <li className="grey">
-                  <a className="btn btn-primary border-radius-5px coming-soon" href='#reserveSpotForm'> Reserve here</a>
-                  </li>
-                </ul>
-               <p> *Article or tutorial related to C#/.NET</p> The entire newsletter issue is written on the topic of the sponsor's product/service so that it fits into the C#/.NET content.
-              </div>
-            </div>
+  return (
+    <>
+      <section className="ftco-about img ftco-section ftco-no-pb sponsorship-header">
+        <div className="container">
+          <div className="pb-5">
+            <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12 heading-section mt-5">
+              <h2 className="mb-4 text-align-center">TheCodeMan .NET Newsletter <br />
+                <span className="text-yellow"><b>Sponsorship</b></span>
+              </h2>
             </div>
           </div>
-          <div className="row pb-3 mt-5">
-            <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-              <h1 className='sponsorship-name'>Example</h1>
-              <div className="row">
-              <div className="col-md-4 mb-5">
-               <p><a href='https://eomail4.com/web-version?p=a57fbf8c-8545-11ef-ae7d-85df232e2a82&pt=campaign&t=1728379993&s=784b945bc08f7b01ba1243c91db3cc19b19e59426863f51d69a39e55bbd61185' target='_blank'> In newsletter</a></p>
-               <p><a href='https://thecodeman.net/posts/how-to-use-singleton-in-multithreading?utm_source=sponsorship' target='_blank'> On website</a></p>
-               </div>
-             <div className='row'>
-             <div className="col-md-12 mb-5">
-             <p>I also offer sponsorships on LinkedIn (90k+ followers) and X (6.5k+) platforms. For more information contact me here.</p>
+          <div className="d-flex">
+            <div className="col-sm-12 col-md-12 col-lg-12 pb-5">
+              <div className="row pb-3">
+                <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                  <h1>Audience</h1>
+                  <br />
+                  <h5>• Reach: <span className="text-yellow">{config.NewsletterSubCount}</span></h5>
+                  {/* <h5>• Growth: <span  className="text-yellow">{config.Growth}</span></h5> */}
+                  {/* <h5>• Number of issues so far: <span  className="text-yellow">{config.IssuesCount}</span></h5> */}
+                  <h5>• Average Open Rate: <span className="text-yellow">{config.OpenRate}</span></h5>
+                  <h5>• Audience: <span className="text-yellow">{config.Audience} </span></h5>
+                  <br />
+                  <h5 className='mb-5'>Reserve your sponsorship: <a className="btn btn-primary border-radius-5px coming-soon" href='#reserveSpotForm'> Reserve here</a></h5>
+                  {/* <h5><b>Currently booked out 2 weeks</b></h5> */}
+                  <h5>Previous Sponsors: <b>{config.PreviousSponsors}</b></h5>
+                </div>
+                <SponsorsNewsletter />
               </div>
+              <div className="row pb-3 mt-5">
+                <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                  <h1 className='sponsorship-name'>Sponsorship opportunities</h1>
+                  <div className="row">
+                    <div className="col-md-4 mt-5 mb-5">
+                      <ul className="price">
+                        <li className="header"><b>Sponsorship</b></li>
+                        <li className="grey" >{config.SharedPrice}</li>
+                        <li>1 of maximum 2 sponsors per issue</li>
+                        <li>Sponsorship will remain live forever on website</li>
+                        <li>2 links per sponsorship</li>
+                        <li>2 sentences maximum</li>
+                        <li className="grey" >
+                          <a className="btn btn-primary border-radius-5px coming-soon" href='#reserveSpotForm'> Reserve here</a>
+                        </li>
+                      </ul>
+                    </div>
+                    <div className="col-md-8 mt-5 mb-5">
+                      <div className="row pb-3 mt-5">
+                        <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                          <h1 className='sponsorship-name'>Example</h1>
+                          <div className="row">
+                            <div className="col-md-12 mb-5">
+                              <p><a href='https://eomail4.com/web-version?p=a57fbf8c-8545-11ef-ae7d-85df232e2a82&pt=campaign&t=1728379993&s=784b945bc08f7b01ba1243c91db3cc19b19e59426863f51d69a39e55bbd61185' target='_blank'> In newsletter</a></p>
+                              <p><a href='https://thecodeman.net/posts/how-to-use-singleton-in-multithreading?utm_source=sponsorship' target='_blank'> On website</a></p>
+                              <p className='mt-5'>I also offer sponsorships on LinkedIn (90k+ followers) and X (6.5k+) platforms.</p>
+                              <p>For more information contact me here.</p>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
               </div>
-              </div>
-            </div>
-            </div>
-          </div>
-        </div>
-    </div>
-  </section>
+              <div className='row'>
+            <div className='col-sm-12 col-md-12 col-lg-12'>
+            <div className="row text-center">
+            <div className="col-md-4"></div>
+            <div className="col-md-8" id='reserveSpotForm'
+              dangerouslySetInnerHTML={{
+                __html: `
+                         <script async src="https://eomail4.com/form/9ade17e6-9c87-11ef-86b3-890d9e639bbe.js" data-form="9ade17e6-9c87-11ef-86b3-890d9e639bbe"></script>
 
-      </>
-    )
-  }
-  
-  export default Sponsorship;
+            `
+              }}
+            />
+            <div className="col-md-2"></div>
+          </div>
+            </div>
+          </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+    </>
+  )
+}
+
+export default Sponsorship;
